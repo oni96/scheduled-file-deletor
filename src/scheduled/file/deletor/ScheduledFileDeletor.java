3b38123a40eb70de5d5d2205d129c6d0cbc06043
oni <oni@oni-HP-2000-Notebook-PC>
2017-09-20 03:04:24 +0530

    Added list for storing time of deletion

--- src/scheduled/file/deletor/ScheduledFileDeletor.java
+++ src/scheduled/file/deletor/ScheduledFileDeletor.java
@@ -16,50 +16,53 @@ public class ScheduledFileDeletor {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
 
         Scanner s = new Scanner(System.in);
         ArrayList<String> fileAddresses = new ArrayList<>();
-
+        ArrayList<String> timeOfDeletion = new ArrayList<>();
+        int fileCount = 0;
+        
         System.out.println("Enter full addresses of the files:");
         String t = s.nextLine();
 
         while (!t.isEmpty()) {
             fileAddresses.add(t);
+            System.out.println("Enter time of deletion in DD/MM/YY hh:mm:ss format:");
+            timeOfDeletion.add(s.nextLine());
             t = s.nextLine();
         }
 
-        System.out.println("Files to delete:" + fileAddresses);
+        System.out.println("Files to delete:");
 
-        System.out.println("Enter time in DD/MM/YYYY hh:mm:ss format:");
-        String timeOfDeletion = s.nextLine();
+        for (int i = 0; i < fileAddresses.size(); i++) {
+            System.out.println(fileAddresses.get(i) + " at " + timeOfDeletion.get(i));
+        }
 
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
 
         while (true) {
             Calendar c = Calendar.getInstance();
             String currentTime = sdf.format(c.getTime());
-            if (currentTime.equals(timeOfDeletion)) {
-                int i = 0;
-                while (i < fileAddresses.size()) {
-                        File file = new File(fileAddresses.get(i++));
-                        if(file.exists())
-                            file.delete();                 
-                        else
-                            System.out.println("File "+ fileAddresses.get(i-1)+ " was not found!");
-                    
+            for (int i = 0; i < fileAddresses.size(); i++) {
+                if (currentTime.equals(timeOfDeletion.get(i))) {
+
+                    File file = new File(fileAddresses.get(i));
+                    if (file.exists()) {
+                        file.delete();
+                        System.out.println(fileAddresses.get(i) + " was deleted at "+ timeOfDeletion.get(i));
+                    } else {
+                        System.out.println("File " + fileAddresses.get(i) + " was not found!");
+                    }
+                    fileCount++;
+                } else {
+                    System.out.print("Waiting...\b\b\b\b\b\b\b\b\b\b");
                 }
-                break;
-            } else {
-                try {
-                    System.out.println(currentTime);
-                    Thread.sleep(1000);
-                } catch (InterruptedException ex) {
-                    Logger.getLogger(ScheduledFileDeletor.class.getName()).log(Level.SEVERE, null, ex);
-                }
+
             }
-
+            if(fileCount==fileAddresses.size())
+                break;
         }
-
     }
 }
+
